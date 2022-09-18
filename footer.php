57c024739d5effbf3b002701a6b3338597e56b39
MOHAMED MOUROUH <mouhamedmourouh1999@gmail.com>
2022-09-18 14:24:57 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,51 @@
+<footer class="pied">
+    <div class="social-media">
+      <span class="fb"><a target="_blank" href="https://www.facebook.com/profile.php?id=100021212841550"><i class="fa-brands fa-facebook-f"></i></a></span>
+      <span class="wtp"><a target="_blank" href="https://chat.whatsapp.com/GkNJ0B38VTn7uFdy0OoNOq"><i class="fa-brands fa-whatsapp"></i></a></span>
+      <span class="wtp"><a target="_blank" href="https://g.page/r/CbgXaezsXQjsEAg/review"><i class="fa-brands fa-google"></i></a></span>
+    </div>
+		<div class="bande1">
+			<div class="row">
+				<div class="col-md-3 navf col-sm-6">
+					<h3>Accès rapide</h3>
+  <ul id="menu_footer" class="menu_footer">
+    <li class="menu-item menu-item-page menu-item-26 active"><a href="index.php" title="Accueil">Accueil</a></li>
+    <li class="menu-item menu-item-gallery menu-item-28"><a href="qui_somme_nous.php" title="Qui sommes-nous ?">Qui sommes-nous ?</a></li>
+    <li class="menu-item menu-item-service.list menu-item-80"><a href="nous_produits.php" title="Nos produits">Nos produits</a></li>
+    <li class="menu-item menu-item-gallery menu-item-32"><a href="nous_realisations.php" title="Nos réalisations">Nos réalisations</a></li>
+    <li class="menu-item menu-item-contact menu-item-34"><a href="contact.php" title="Contact">Contact</a></li>
+  </ul>
+
+				</div>
+				<div class="col-md-4 col-sm-6 nopad">
+					<h3>Contactez-nous</h3>
+					<address>
+            
+						<p class="adresse">
+							 <span class="texte">Boulevard Mokhtar soussi </span><br><span class="texte">sidi maarouf 20670 </span><span class="texte">Casablanca</span></p>
+						<p class="tel tel1 callTracker"><span class="t_phone">06 75 63 15 00</span></p>
+						<p class="tel tel1 callTracker"><span class="t_phone">06 94 84 83 84</span></p>
+					</address>
+										<h3>Nos horaires</h3>
+					<p class="horaires mb-5">Disponible 24/24 7j/7</p>
+									</div>
+				<div class="col-md-5 col-sm-12 nopad text-left ">
+				<p class='footer-logo'></p>
+          <p class="">Notre professionnalisme, notre expérience et notre expertises vous garantissent des produits et des solutions de qualité.</p>
+        </div>
+</footer>
+
+
+      <!-- AOS CDN -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <!-- <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>   -->
+      <!-- full screen lightbox library -->
+      <script src="./node_modules/fslightbox/index.js"></script>
+    <!-- Swiper slider cdn -->
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <!-- Bootstrap cdn -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- Script file -->
+    <script src="./static/js/script.js"></script>
+</body>
+</html>
